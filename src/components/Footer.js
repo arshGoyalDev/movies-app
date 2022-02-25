1fec5cdd0ccb779a22d0213fae6d952501e08e23
arshWebDev <arshgoyalDev@gmail.com>
2022-02-25 19:42:59 +0530

    fixed a issue in link

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -12,7 +12,7 @@ const Footer = () => {
           </p>
 
           <div className="footer--attribution--api">
-            <a href="https://www.themoviedb.org" target="_blank">
+            <a href="https://www.themoviedb.org" target="_blank" rel="noreferrer noopener">
               <img
                 src="https://www.themoviedb.org/assets/2/v4/logos/v2/blue_long_2-9665a76b1ae401a510ec1e0ca40ddcb3b0cfe45f1d51b77a308fea0845885648.svg"
                 alt="tmdb logo"
