36caef4c8958b52040f5fae0faabc34a01be6c3f
arshWebDev <arshgoyalDev@gmail.com>
2022-02-26 16:17:08 +0530

    provided attribution for illustration

--- src/components/Error.js
+++ src/components/Error.js
@@ -1,16 +1,50 @@
 import "./styles/Error.scss";
 
+import { Link } from "react-router-dom";
+
 import ErrorSvg from "../assets/images/error.svg";
-import ErrorLargeSvg from '../assets/images/error-large.svg';
+import ErrorLargeSvg from "../assets/images/error-large.svg";
 
 const Error = () => {
   return (
     <div className="error">
-      <img src={ErrorSvg} alt="error-404" className="small" />
-      <img src={ErrorLargeSvg} alt="error-404" className="large" />
+      <div className="small">
+        <img src={ErrorSvg} alt="error-404" />
+        <div className="attribution">
+          <p>
+            illustration by
+            <a
+              href="https://storyset.com/online"
+              target="_blank"
+              rel="noreferrer noopener"
+            >
+              {" "}
+              Storyset
+            </a>{" "}
+          </p>
+        </div>
+      </div>
+      <div className="large">
+        <img src={ErrorLargeSvg} alt="error-404" />
+        <div className="attribution">
+          <p>
+            illustration by
+            <a
+              href="https://storyset.com/internet"
+              target="_blank"
+              rel="noreferrer noopener"
+            >
+              {" "}
+              Storyset
+            </a>{" "}
+          </p>
+        </div>
+      </div>
       <div className="error--actions">
         <h1 className="error--message">Page Not Found</h1>
-        <button className="home-btn">Back to Home</button>
+        <Link to="/">
+          <button className="home-btn">Back to Home</button>
+        </Link>
       </div>
     </div>
   );
