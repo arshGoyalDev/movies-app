a55f26fb033e57ffd858415e69ea45a743f9ac56
arshWebDev <arshgoyalDev@gmail.com>
2022-03-14 19:59:04 +0530

    added some images section in profile details

--- src/components/more/ProfileDetails.js
+++ src/components/more/ProfileDetails.js
@@ -18,13 +18,14 @@ const ProfileDetails = () => {
   const dispatch = useDispatch();
   const { id } = useSelector((state) => state.profileDetails.value);
   const data = useFetch(`person/${id}?language=en-US&`, false);
+  const images = useFetch(`person/${id}}/images?`, "profiles");
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    if (data) {
+    if (data && images) {
       setLoading(false);
     }
-  }, [data]);
+  }, [data, images]);
 
   const closeProfileDetails = () => {
     dispatch(clearProfileDetails());
@@ -45,27 +46,52 @@ const ProfileDetails = () => {
                 <FontAwesomeIcon icon={faXmark} />
               </button>
             </div>
-            <div className="introduction">
-              {data.profile_path ? (
-                <img
-                  loading="lazy"
-                  src={`https://image.tmdb.org/t/p/w500${data.profile_path}`}
-                  alt={data.name}
-                />
-              ) : (
-                <FontAwesomeIcon icon={faUser} />
-              )}
-              <div className="introduction--details">
-                <h2>{data.name}</h2>
-                {data.biography && (
-                  <p className="biography">{data.biography}</p>
+            <div className="profile-details--body">
+              <div className="introduction">
+                {data.profile_path ? (
+                  <img
+                    loading="lazy"
+                    src={`https://image.tmdb.org/t/p/w500${data.profile_path}`}
+                    alt={data.name}
+                  />
+                ) : (
+                  <div className="no-images">
+                    <FontAwesomeIcon icon={faUser} />
+                  </div>
                 )}
-                {data.birthday && <p>Born on: <span>{modifyDate(data.birthday)}</span></p>}
-                {data.place_of_birth && (
-                  <p>Place of birth: <span>{data.place_of_birth}</span></p>
-                )}
-                <p>Known for: <span>{data.known_for_department}</span></p>
+                <div className="introduction--details">
+                  <h2>{data.name}</h2>
+                  {data.biography && (
+                    <p className="biography">{data.biography}</p>
+                  )}
+                  {data.birthday && (
+                    <p>
+                      Born on: <span>{modifyDate(data.birthday)}</span>
+                    </p>
+                  )}
+                  {data.place_of_birth && (
+                    <p>
+                      Place of birth: <span>{data.place_of_birth}</span>
+                    </p>
+                  )}
+                  <p>
+                    Known for: <span>{data.known_for_department}</span>
+                  </p>
+                </div>
               </div>
+              {images.length > 1 && (
+                <div className="images">
+                  <h3>Some Images</h3>
+                  <div>
+                    {images.slice(1, images.length).map((image) => (
+                      <img
+                        key={image.file_path}
+                        src={`https://image.tmdb.org/t/p/w500${image.file_path}`}
+                      />
+                    ))}
+                  </div>
+                </div>
+              )}
             </div>
           </>
         ) : (
