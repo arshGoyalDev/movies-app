84bff450a64899b79606515800c565f61a317a99
arshWebDev <arshgoyalDev@gmail.com>
2022-03-17 23:24:25 +0530

    removed a console.log()

--- src/components/Details/Recommended.js
+++ src/components/Details/Recommended.js
@@ -1,7 +1,6 @@
 import { BackdropCard } from "../cards";
 
 const Recommended = ({ data, query }) => {
-  console.log(query);
   return (
     <div className="details--recommended">
       {data.length !== 0 && (
