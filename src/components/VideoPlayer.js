be91b372dc3685f85de2cc29372c7f7ae48ec316
arshWebDev <arshgoyalDev@gmail.com>
2022-03-18 00:00:38 +0530

    added coontrol to youtube embedded video

--- src/components/VideoPlayer.js
+++ src/components/VideoPlayer.js
@@ -30,7 +30,7 @@ const VideoPlayer = () => {
         <iframe
           width="560"
           height="315"
-          src={`https://www.youtube.com/embed/${key}?controls=0`}
+          src={`https://www.youtube.com/embed/${key}`}
           title="YouTube video player"
           frameBorder="0"
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
