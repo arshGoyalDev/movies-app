f6e4031898dbec055e43a16c32245e0c63fb94f3
arshWebDev <arshgoyalDev@gmail.com>
2022-03-17 22:52:21 +0530

    restyled mobile navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -5,11 +5,11 @@ import "./styles/NavBar.scss";
 import { Link, useNavigate } from "react-router-dom";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faBars } from "@fortawesome/free-solid-svg-icons";
+import { faBars, faXmark } from "@fortawesome/free-solid-svg-icons";
 
 const NavBar = () => {
   const navigate = useNavigate();
-  
+
   const [searchTerm, setSearchTerm] = useState("");
   const [menuVisible, setMenuVisible] = useState(false);
 
@@ -33,36 +33,46 @@ const NavBar = () => {
       >
         <FontAwesomeIcon icon={faBars} />{" "}
       </button>
-      <div className={`nav--menu ${menuVisible && "active"}`}>
-        <ul className="nav--menu--list">
-          <li>
-            <Link to="/movie">
-              <button onClick={() => setMenuVisible(false)}>Movies</button>
-            </Link>
-          </li>
-          <li>
-            <Link to="/tv">
-              <button onClick={() => setMenuVisible(false)}>Tv Shows</button>
-            </Link>
-          </li>
-        </ul>
+      <div className={`nav--menu--wrapper ${menuVisible && "wrapper-active"}`}>
+        <button className={`nav--menu--close-btn ${menuVisible && "close-btn-active"}`} onClick={() => setMenuVisible(false)}>
+          <FontAwesomeIcon icon={faXmark} />
+        </button>
+        <div className={`nav--menu ${menuVisible && "menu-active"}`}>
+          <ul className="nav--menu--list">
+            <li>
+              <Link to="/">
+                <button onClick={() => setMenuVisible(false)}>Home</button>
+              </Link>
+            </li>
+            <li>
+              <Link to="/movie">
+                <button onClick={() => setMenuVisible(false)}>Movies</button>
+              </Link>
+            </li>
+            <li>
+              <Link to="/tv">
+                <button onClick={() => setMenuVisible(false)}>Tv Shows</button>
+              </Link>
+            </li>
+          </ul>
 
-        <div className="nav--menu--search">
-          <input
-            type="input"
-            className="nav--menu--search--input"
-            placeholder="search..."
-            value={searchTerm}
-            onChange={(e) => {
-              setSearchTerm(e.target.value);
-            }}
-            onKeyDown={(e) => {
-              e.keyCode === 13 && search();
-            }}
-          />
-          <button className="nav--menu--search--btn" onClick={search}>
-            Search
-          </button>
+          <div className="nav--menu--search">
+            <input
+              type="input"
+              className="nav--menu--search--input"
+              placeholder="search..."
+              value={searchTerm}
+              onChange={(e) => {
+                setSearchTerm(e.target.value);
+              }}
+              onKeyDown={(e) => {
+                e.keyCode === 13 && search();
+              }}
+            />
+            <button className="nav--menu--search--btn" onClick={search}>
+              Search
+            </button>
+          </div>
         </div>
       </div>
     </nav>
