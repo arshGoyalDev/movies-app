08484141934f67ca6fedcf23c72ca81d8d679fab
arshWebDev <arshgoyalDev@gmail.com>
2022-04-30 22:57:57 +0530

    set-up react-router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import './App.css';
 
+import { Route, Routes } from 'react-router-dom';
+
+import { Home } from './pages';
+
+
 const App = () => {
   return (
     <div className="App">
-      APP
+      <Routes>
+        <Route path='/' element={<Home />} />
+      </Routes>
     </div>
   );
 }
