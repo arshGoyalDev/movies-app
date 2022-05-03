6fbdec92ee21a489d4d9a545378732bc5294024b
arshWebDev <arshgoyalDev@gmail.com>
2022-05-03 22:19:34 +0530

    add: functionality to collapse sidemenu

--- src/components/sections/SideMenu.js
+++ src/components/sections/SideMenu.js
@@ -1,5 +1,8 @@
+import { useState } from "react";
+
 import { Link, useLocation } from "react-router-dom";
 
+import menuBurger from "../../assets/images/menu-burger.svg";
 import home from "../../assets/images/home.svg";
 import movies from "../../assets/images/movies.svg";
 import tvShows from "../../assets/images/tv-shows.svg";
@@ -9,22 +12,47 @@ import star from "../../assets/images/star.svg";
 import genres from "../../assets/images/genres.svg";
 
 const SideMenu = () => {
+  const [menuOpen, setMenuOpen] = useState(true);
   const location = useLocation();
 
   return (
-    <div className="hidden xl:flex flex-col w-80 py-6 border-r-2 border-solid border-gray-300 dark:border-neutral-800">
-      <Link to="/">
-        <p className="text-2xl font-medium tracking-widest px-6">Movies.info</p>
-      </Link>
+    <div
+      className={`hidden xl:flex flex-col ${
+        menuOpen ? "w-80" : "w-20"
+      } py-6 border-r-2 border-solid border-gray-300 dark:border-neutral-800 transition-all duration-300`}
+    >
+      <div className={`flex items-center gap-4 h-10 ${menuOpen ? "gap-4 px-6" : "px-[26px] gap-0"} transition-all duration-300`}>
+        <button
+          onClick={() => setMenuOpen(!menuOpen)}
+          className="w-6 h-4 flex flex-col justify-between"
+        >
+          <span className="block w-6 h-0.5 bg-white"></span>
+          <span className="block w-6 h-0.5 bg-white"></span>
+          <span className="block w-6 h-0.5 bg-white"></span>
+        </button>
+        <Link to="/">
+          <p
+            className={`text-2xl tracking-widest font-medium ${
+              !menuOpen && "scale-x-0 w-0"
+            } origin-left transition-all duration-300`}
+          >
+            Movies.info
+          </p>
+        </Link>
+      </div>
 
       <div className="mt-12">
-        <span className="text-xs font-bold text-gray-400 dark:text-neutral-500 px-6">
+        <span
+          className={`text-xs font-bold text-gray-400 dark:text-neutral-500 ${
+            menuOpen ? "px-6" : "px-[22px]"
+          } transition-all duration-300`}
+        >
           Menu
         </span>
         <ul className="flex flex-col gap-4 mt-4">
           <Link to="/">
             <li
-              className={`flex items-center gap-4 py-1.5 px-6 border-l-4 border-solid ${
+              className={`flex items-center gap-4 h-10 py-1.5 px-6 border-l-4 border-solid ${
                 location.pathname === "/"
                   ? "border-primary-light dark:border-primary-dark"
                   : "border-transparent"
@@ -35,12 +63,18 @@ const SideMenu = () => {
                 alt="home icon by uicons"
                 className="w-5 dark:invert"
               />
-              <span className="font-medium">Home</span>
+              <span
+                className={`font-medium ${
+                  !menuOpen && "scale-x-0 w-0"
+                } origin-left transition-all duration-300`}
+              >
+                Home
+              </span>
             </li>
           </Link>
           <Link to="/movies">
             <li
-              className={`flex items-center gap-4 py-1.5 px-6 border-l-4 border-solid ${
+              className={`flex items-center gap-4 h-10 py-1.5 px-6 border-l-4 border-solid ${
                 location.pathname === "/movies"
                   ? "border-primary-light dark:border-primary-dark"
                   : "border-transparent"
@@ -51,12 +85,18 @@ const SideMenu = () => {
                 alt="movies icon by uicons"
                 className="w-5 dark:invert"
               />
-              <span className="font-medium">Movies</span>
+              <span
+                className={`font-medium ${
+                  !menuOpen && "scale-x-0 w-0"
+                } origin-left transition-all duration-300`}
+              >
+                Movies
+              </span>
             </li>
           </Link>
           <Link to="/tv-shows">
             <li
-              className={`flex items-center gap-4 py-1.5 px-6 border-l-4 border-solid ${
+              className={`flex items-center gap-4 h-10 py-1.5 px-6 border-l-4 border-solid ${
                 location.pathname === "/tv-shows"
                   ? "border-primary-light dark:border-primary-dark"
                   : "border-transparent"
@@ -67,12 +107,18 @@ const SideMenu = () => {
                 alt="tv icon by uicons"
                 className="w-5 dark:invert"
               />
-              <span className="font-medium">Tv Shows</span>
+              <span
+                className={`font-medium ${
+                  !menuOpen && "scale-x-0 w-0"
+                } origin-left transition-all duration-300`}
+              >
+                Tv Shows
+              </span>
             </li>
           </Link>
           <Link to="/people">
             <li
-              className={`flex items-center gap-4 py-1.5 px-6 border-l-4 border-solid ${
+              className={`flex items-center gap-4 h-10 py-1.5 px-6 border-l-4 border-solid ${
                 location.pathname === "/people"
                   ? "border-primary-light dark:border-primary-dark"
                   : "border-transparent"
@@ -83,19 +129,29 @@ const SideMenu = () => {
                 alt="people icon by uicons"
                 className="w-5 dark:invert"
               />
-              <span className="font-medium">People</span>
+              <span
+                className={`font-medium ${
+                  !menuOpen && "scale-x-0 w-0"
+                } origin-left transition-all duration-300`}
+              >
+                People
+              </span>
             </li>
           </Link>
         </ul>
       </div>
       <div className="mt-8">
-        <span className="text-xs font-bold text-gray-400 dark:text-neutral-500 px-6">
+        <span
+          className={`text-xs font-bold text-gray-400 dark:text-neutral-500 ${
+            menuOpen ? "px-6" : "px-[22px]"
+          } transition-all duration-300`}
+        >
           More
         </span>
         <ul className="flex flex-col gap-4 mt-4">
           <Link to="/upcoming">
             <li
-              className={`flex items-center gap-4 py-1.5 px-6 border-l-4 border-solid ${
+              className={`flex items-center gap-4 h-10 py-1.5 px-6 border-l-4 border-solid ${
                 location.pathname === "/upcoming"
                   ? "border-primary-light dark:border-primary-dark"
                   : "border-transparent"
@@ -106,12 +162,18 @@ const SideMenu = () => {
                 alt="home icon by uicons"
                 className="w-5 dark:invert"
               />
-              <span className="font-medium">Upcoming</span>
+              <span
+                className={`font-medium ${
+                  !menuOpen && "scale-x-0 w-0"
+                } origin-left transition-all duration-300`}
+              >
+                Upcoming
+              </span>
             </li>
           </Link>
           <Link to="/top-rated">
             <li
-              className={`flex items-center gap-4 py-1.5 px-6 border-l-4 border-solid ${
+              className={`flex items-center gap-4 h-10 py-1.5 px-6 border-l-4 border-solid ${
                 location.pathname === "/top-rated"
                   ? "border-primary-light dark:border-primary-dark"
                   : "border-transparent"
@@ -122,12 +184,18 @@ const SideMenu = () => {
                 alt="movies icon by uicons"
                 className="w-5 dark:invert"
               />
-              <span className="font-medium">Top Rated</span>
+              <span
+                className={`font-medium ${
+                  !menuOpen && "scale-x-0 w-0"
+                } origin-left transition-all duration-300`}
+              >
+                Top Rated
+              </span>
             </li>
           </Link>
           <Link to="/genres">
             <li
-              className={`flex items-center gap-4 py-1.5 px-6 border-l-4 border-solid ${
+              className={`flex items-center gap-4 h-10 py-1.5 px-6 border-l-4 border-solid ${
                 location.pathname === "/genres"
                   ? "border-primary-light dark:border-primary-dark"
                   : "border-transparent"
@@ -138,7 +206,13 @@ const SideMenu = () => {
                 alt="tv icon by uicons"
                 className="w-5 dark:invert"
               />
-              <span className="font-medium">Genres</span>
+              <span
+                className={`font-medium ${
+                  !menuOpen && "scale-x-0 w-0"
+                } origin-left transition-all duration-300`}
+              >
+                Genres
+              </span>
             </li>
           </Link>
         </ul>
