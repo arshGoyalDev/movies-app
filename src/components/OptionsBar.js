01e1f877eec68a949ce64f3ed4bede3a7f4896fc
arshWebDev <arshgoyalDev@gmail.com>
2022-05-05 16:49:29 +0530

    add: options bar for small devices

---
+++ src/components/OptionsBar.js
@@ -0,0 +1,26 @@
+import { Link } from "react-router-dom";
+
+const OptionsBar = () => {
+  return (
+    <div className="flex gap-3 px-5 md:px-16 py-1 overflow-x-auto">
+      <Link to="/genres">
+        <p className="text-sm min-w-[80px] text-center py-1 px-3 bg-gray-200 dark:bg-neutral-800 rounded-full">
+          Genres
+        </p>
+      </Link>
+
+      <Link to="/upcoming">
+        <p className="text-sm min-w-[100px] text-center py-1 px-3 bg-gray-200 dark:bg-neutral-800 rounded-full">
+          Upcoming
+        </p>
+      </Link>
+      <Link to="/top-rated">
+        <p className="text-sm min-w-[110px] text-center py-1 px-3 bg-gray-200 dark:bg-neutral-800 rounded-full">
+          Top Rated
+        </p>
+      </Link>
+    </div>
+  );
+};
+
+export default OptionsBar;
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,5 +1,7 @@
 import { BottomNav, NavBar, SideMenu } from "../components/sections";
 
+import OptionsBar from "../components/OptionsBar";
+
 const Home = () => {
   return (
     <main className="flex min-h-screen bg-red">
@@ -7,6 +9,7 @@ const Home = () => {
       <div className="w-full">
         <BottomNav />
         <NavBar />
+        <OptionsBar />
       </div>
     </main>
   );
