2f9deab9633f5f396ba853576817fc40470a6e59
arshWebDev <arshgoyalDev@gmail.com>
2022-05-07 22:55:00 +0530

    add: shortcut indication in the search bar

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -6,7 +6,10 @@ const SearchBar = () => {
   const [searchValue, setSearchValue] = useState("");
 
   return (
-    <div className="flex items-center bg-gray-100 dark:bg-neutral-800 rounded">
+    <div className="relative flex items-center bg-gray-100 dark:bg-neutral-800 rounded-md">
+      <button className="ml-2 w-10 h-10 grid place-items-center">
+        <SearchIcon className="icon w-4 h-4" />
+      </button>
       <div className="relative">
         <input
           type="text"
@@ -17,16 +20,22 @@ const SearchBar = () => {
           className="w-96 py-3 px-4 text-sm bg-transparent focus:outline-none"
         />
         <span
-          className={`placeholder-text absolute left-0 top-1/2 -translate-y-1/2 w-full pointer-events-none text-sm text-gray-500 dark:text-neutral-500 px-4 ${
+          className={`placeholder-text absolute left-0 top-1/2 -translate-y-1/2 w-full pointer-events-none text-sm text-gray-500 dark:text-neutral-500 ${
             searchValue !== "" && "opacity-0 left-4"
-          } transition-all duration-300`}
+          } px-2 transition-all duration-300`}
         >
           Search for movies, tv-shows or people
         </span>
       </div>
-      <button className="w-16 h-10 grid place-items-center">
-        <SearchIcon className="icon w-4 h-4" />
-      </button>
+
+      <div className="absolute right-3 flex gap-1.5">
+        <span className="font-semibold py-0.5 px-2 bg-gray-300 dark:bg-neutral-900 rounded">
+          Ctrl
+        </span>
+        <span className="font-semibold py-0.5 px-2 bg-gray-300 dark:bg-neutral-900 rounded">
+          K
+        </span>
+      </div>
     </div>
   );
 };
