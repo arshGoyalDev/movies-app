8e385eed7398341ad6a0ba278cff0ff012fd83c6
arshWebDev <arshgoyalDev@gmail.com>
2022-05-10 21:49:59 +0530

    completed search backdrop card

--- src/components/cards/SearchBackdropCard.js
+++ src/components/cards/SearchBackdropCard.js
@@ -1,34 +1,25 @@
-import Rating from "../Rating";
-
-import { ImageIcon, MenuDotsIcon, StarIcon } from "../icons";
+import { ImageIcon } from "../icons";
 
 const SearchBackdropCard = ({ data }) => {
   return (
-    <div className="flex gap-4 min-w-[300px] bg-gray-200 dark:bg-neutral-700 rounded-lg">
-      <div className="grid place-items-center bg-gray-300 dark:bg-neutral-600 w-[120px] h-full rounded-lg overflow-hidden">
+    <div className="relative min-w-[240px] h-32 bg-gray-200 dark:bg-neutral-700 rounded-lg overflow-hidden">
+      <div className="absolute z-[1] w-full h-full grid place-items-center">
         {data.backdrop_path ? (
           <img
-            loading="lazy"
-            src={`https://image.tmdb.org/t/p/w500${data.poster_path}`}
-            alt={data.title ?? data.name}
-            className="w-full h-full"
+            src={`https://image.tmdb.org/t/p/w500${data.backdrop_path}`}
+            alt="backdrop"
+            className="w-full"
           />
-
         ) : (
           <ImageIcon className="icon w-10 h-10" />
         )}
       </div>
-      <div className="flex flex-col justify-between items-start py-4">
-        <p className="dark:text-white font-medium">{(data.title ?? data.name).length <= 20 ? (data.title ?? data.name) : `${(data.title ?? data.name).slice(0, 20)}...`}</p>
-        <div>
-          <div className="flex items-center gap-3">
-          <StarIcon className="rating-icon w-4 h-4" />
-          <span className="text-primary-light dark:text-primary-dark">{data.vote_average}</span>
-          </div>
-        </div>
-        <button className="grid place-items-center min-w-[40px] h-10 bg-gray-300 dark:bg-neutral-600 rounded-full">
-          <MenuDotsIcon className="icon w-4 h-4" />
-        </button>
+      <div className="absolute z-[2] w-full h-full flex items-end p-4 bg-black bg-opacity-40">
+        <p className="text-white font-medium">
+          {(data.title ?? data.name).length <= 20
+            ? data.title ?? data.name
+            : `${(data.title ?? data.name).slice(0, 20)}...`}
+        </p>
       </div>
     </div>
   );
