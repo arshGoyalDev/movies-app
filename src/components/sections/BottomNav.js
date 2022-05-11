03a72e2dfc679e0daec6ebe2697137dbf49f31e2
arshWebDev <arshgoyalDev@gmail.com>
2022-05-11 22:17:17 +0530

    fix some issues for mobile devices

--- src/components/sections/BottomNav.js
+++ src/components/sections/BottomNav.js
@@ -8,7 +8,7 @@ const BottomNav = () => {
   return (
     <nav className="sm:hidden fixed bottom-0 w-full flex justify-between items-center py-5 px-6 dark:bg-neutral-800 rounded-t-[30px] shadow-[0_-10px_60px_rgba(100,100,100,0.4)] dark:shadow-none">
       <Link to="/">
-        <div className="grid place-items-center w-16 h-10">
+        <div className="grid place-items-center w-20 h-10">
           <HomeIcon
             className={`icon w-6 h-6 ${
               location.pathname !== "/" ? "block" : "hidden"
@@ -25,7 +25,7 @@ const BottomNav = () => {
         </div>
       </Link>
       <Link to="/movies">
-        <div className="grid place-items-center w-16 h-10">
+        <div className="grid place-items-center w-20 h-10">
           <MoviesIcon
             className={`icon w-6 h-6 ${
               location.pathname !== "/movies" ? "block" : "hidden"
@@ -36,13 +36,13 @@ const BottomNav = () => {
               location.pathname === "/movies" ? "flex" : "hidden"
             }`}
           >
-            <span className="font-medium">Home</span>
+            <span className="font-medium">Movies</span>
             <span className="block w-5 h-1 bg-neutral-700 dark:bg-gray-400 rounded-md"></span>
           </div>
         </div>
       </Link>
       <Link to="/tv-shows">
-        <div className="grid place-items-center w-16 h-10">
+        <div className="grid place-items-center w-20 h-10">
           <TvIcon
             className={`icon w-6 h-6 ${
               location.pathname !== "/tv-shows" ? "block" : "hidden"
@@ -53,13 +53,13 @@ const BottomNav = () => {
               location.pathname === "/tv-shows" ? "flex" : "hidden"
             }`}
           >
-            <span className="font-medium">Home</span>
+            <span className="font-medium">TV Shows</span>
             <span className="block w-5 h-1 bg-neutral-700 dark:bg-gray-400 rounded-md"></span>
           </div>
         </div>
       </Link>
       <Link to="/people">
-        <div className="grid place-items-center w-16 h-10">
+        <div className="grid place-items-center w-20 h-10">
           <PeopleIcon
             className={`icon w-6 h-6 ${
               location.pathname !== "/people" ? "block" : "hidden"
@@ -70,7 +70,7 @@ const BottomNav = () => {
               location.pathname === "/people" ? "flex" : "hidden"
             }`}
           >
-            <span className="font-medium">Home</span>
+            <span className="font-medium">People</span>
             <span className="block w-5 h-1 bg-neutral-700 dark:bg-gray-400 rounded-md"></span>
           </div>
         </div>
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,8 +1,10 @@
+import OptionsBar from '../components/OptionsBar'
 import Trending from "../components/Trending";
 
 const Home = () => {
   return (
-    <div className="px-5 md:px-16 xl:pr-24 xl:pl-10 mt-6">
+    <div className="px-5 md:px-16 xl:pr-24 xl:pl-10 mt-0">
+      <OptionsBar />
       <Trending />
     </div>
   );
