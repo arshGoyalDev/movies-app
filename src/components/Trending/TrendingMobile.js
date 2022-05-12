63e6ae578b38399b590787684190f95d67b67083
arshWebDev <arshgoyalDev@gmail.com>
2022-05-12 11:56:31 +0530

    fix a issue in trending mobile

--- src/components/Trending/TrendingMobile.js
+++ src/components/Trending/TrendingMobile.js
@@ -4,12 +4,12 @@ const TrendingMobile = ({ data, activeNum, setActiveNum }) => {
   // console.log(data[activeNum - 1].poster_path ?? data[19].poster_path);
 
   return (
-    <div className="relative sm:hidden h-80 mt-10 overflow-hidden">
+    <div className="relative sm:hidden h-[430px] mt-10 overflow-hidden">
       <div
         onClick={() => setActiveNum(data[activeNum - 1] ? activeNum - 1 : 19)}
-        className="absolute min-w-[180px] h-max rounded-xl -translate-x-[70%] -rotate-6 translate-y-16 overflow-hidden shadow-2xl dark:shadow-none shadow-gray-500"
+        className="absolute min-w-[220px] h-max rounded-2xl -translate-x-[75%] -rotate-6 translate-y-16 overflow-hidden shadow-2xl dark:shadow-none shadow-gray-500"
       >
-        <div className="w-full h-60 opacity-80 dark:opacity-70">
+        <div className="w-full h-80 opacity-80 dark:opacity-70">
           <img
             src={`https://image.tmdb.org/t/p/w500${
               data[activeNum - 1]
@@ -23,9 +23,9 @@ const TrendingMobile = ({ data, activeNum, setActiveNum }) => {
       </div>
 
       <div
-        className={`absolute min-w-[180px] h-max rounded-xl left-1/2 -translate-x-1/2 overflow-hidden shadow-2xl dark:shadow-none shadow-gray-500 bg-neutral-800`}
+        className={`absolute min-w-[220px] h-max rounded-2xl left-1/2 -translate-x-1/2 overflow-hidden shadow-2xl dark:shadow-none shadow-gray-500 bg-neutral-800`}
       >
-        <div className="w-full h-64">
+        <div className="w-full h-80">
           <img
             src={`https://image.tmdb.org/t/p/w500${data[activeNum].poster_path}`}
             alt="backdrop"
@@ -36,9 +36,9 @@ const TrendingMobile = ({ data, activeNum, setActiveNum }) => {
 
       <div
         onClick={() => setActiveNum(data[activeNum + 1] ? activeNum + 1 : 0)}
-        className={`absolute min-w-[180px] h-max rounded-xl right-0 translate-x-[70%] rotate-6 translate-y-16 overflow-hidden shadow-2xl dark:shadow-none shadow-gray-500`}
+        className={`absolute min-w-[220px] h-max rounded-2xl right-0 translate-x-[75%] rotate-6 translate-y-16 overflow-hidden shadow-2xl dark:shadow-none shadow-gray-500`}
       >
-        <div className="w-full h-60 opacity-80 dark:opacity-70">
+        <div className="w-full h-80 opacity-80 dark:opacity-70">
           <img
             src={`https://image.tmdb.org/t/p/w500${
               data[activeNum + 1]
