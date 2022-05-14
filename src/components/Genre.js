11172df8a6e470aaf0e0523e8b03856b8ef32d31
arshWebDev <arshgoyalDev@gmail.com>
2022-05-14 18:48:36 +0530

    fix: issue in font-size

--- src/components/Genre.js
+++ src/components/Genre.js
@@ -14,7 +14,7 @@ const Genre = ({ genreId, list }) => {
   }, []);
 
   return (
-    <div className="text-black dark:text-white text-sm py-1 px-2 bg-gray-200 dark:bg-neutral-800 bg-opacity-60 dark:bg-opacity-70 rounded">
+    <div className="font-medium text-black dark:text-white text-sm py-1 px-2 bg-gray-200 dark:bg-neutral-800 bg-opacity-60 dark:bg-opacity-70 rounded">
       {output}
     </div>
   );
