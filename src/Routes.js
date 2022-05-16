63e21c7f244c860f81ea2206936c1e145e0dc28f
arshWebDev <arshgoyalDev@gmail.com>
2022-05-16 19:25:38 +0530

    fix: error in routes

--- src/Routes.js
+++ src/Routes.js
@@ -1,22 +1,16 @@
-import { Routes as RoutesWrapper, Route, useLocation } from "react-router-dom";
-
-import { AnimatePresence } from "framer-motion";
+import { Routes as RoutesWrapper, Route } from "react-router-dom";
 
 import { Home, Movies } from "./pages";
 
 const Routes = () => {
-  const location = useLocation();
-
   return (
-    <AnimatePresence>
-      <RoutesWrapper location={location} key={location.pathname}>
-        <Route path="/" element={<Home />} />
+    <RoutesWrapper>
+      <Route path="/" element={<Home />} />
 
-        <Route path="/movies" element={<Movies />}></Route>
+      <Route path="/movies" element={<Movies />}></Route>
 
-        <Route path="*" element={<div>Nothing to see here</div>} />
-      </RoutesWrapper>
-    </AnimatePresence>
+      <Route path="*" element={<div>Nothing to see here</div>} />
+    </RoutesWrapper>
   );
 };
 
