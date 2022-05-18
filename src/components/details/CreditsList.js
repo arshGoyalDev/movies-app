c9b64b95ebdd83cfd78ec4083ab6932384a2b740
arshWebDev <arshgoyalDev@gmail.com>
2022-05-18 22:23:44 +0530

    styled details page for large devices

--- src/components/details/CreditsList.js
+++ src/components/details/CreditsList.js
@@ -2,12 +2,11 @@ import { PersonCard } from "../cards";
 
 const CreditsList = ({ data, heading }) => {
   return (
-    <div className="px-1">
-      <h4 className="capitalize font-medium px-9">{heading}</h4>
-      <div className="flex gap-3 px-9 mt-3 overflow-auto">
+    <div>
+      <h4 className="capitalize font-medium px-10 md:px-28 lg:px-0">{heading}</h4>
+      <div className="flex gap-3 px-10 md:px-28 lg:px-0 mt-3 overflow-auto">
         {data.map((castMember) => (
           <PersonCard key={castMember.credit_id} data={castMember} />
-          // console.log(castMember)
         ))}
       </div>
     </div>
--- src/pages/Details.js
+++ src/pages/Details.js
@@ -5,6 +5,7 @@ import { useNavigate, useParams } from "react-router-dom";
 
 import { StarSolidIcon, ArrowLeftIcon } from "../components/icons";
 import { CreditsList } from "../components/details";
+import { convertMinsToHrsMins, modifyDate } from "../utils/time";
 
 const Details = ({ type }) => {
   const { id } = useParams();
@@ -17,6 +18,7 @@ const Details = ({ type }) => {
   useEffect(() => {
     if (data && credits) {
       setLoading(false);
+      console.log(data);
     }
   }, [data, credits]);
 
@@ -25,16 +27,20 @@ const Details = ({ type }) => {
       {loading ? (
         ""
       ) : (
-        <main className="pt-6 pb-16">
-          {/* <button
-            onClick={() => navigate(-1)}
-            className="grid place-items-center w-8 h-8 mx-5 rounded-full"
-          >
-            <ArrowLeftIcon className="icon w-4 h-4" />
-          </button>
- */}
-          <div className="flex flex-col gap-10 items-center mt-10">
-            <div className="relative z-20 w-[60%] rounded-2xl">
+        <main className="pt-6 pb-16 lg:pt-0">
+          <div className="relative hidden lg:block h-[300px] overflow-hidden rounded-bl-[50px]">
+            <div className="absolute z-[1] w-full h-full">
+              <img
+                loading="lazy"
+                src={`https://image.tmdb.org/t/p/w500${data.backdrop_path}`}
+                alt={data.name}
+                className="w-full h-[600px] mt-[-150px]"
+              />
+            </div>
+            <div className="absolute z-[2] w-full h-full bg-black bg-opacity-30"></div>
+          </div>
+          <div className="flex flex-col lg:flex-row gap-10 md:gap-20 lg:gap-6 items-center lg:items-start lg:px-36 mt-10 ">
+            <div className="relative z-20 w-[60%] lg:min-w-[200px] max-w-[340px] lg:max-w-[200px] rounded-2xl">
               <div className="absolute z-[-1] rounded-2xl bg-gray-300 dark:bg-neutral-700 w-[95%] h-[103%] top-0 left-1/2 -translate-x-1/2"></div>
               <div className="absolute z-[-2] rounded-2xl bg-gray-200 dark:bg-neutral-800 w-[90%] h-[106%] top-0 left-1/2 -translate-x-1/2"></div>
               <img
@@ -45,16 +51,21 @@ const Details = ({ type }) => {
               />
             </div>
 
-            <div className="w-full px-10">
-              <h1 className="text-xl font-medium">{data.title ?? data.name}</h1>
-              <div className="flex gap-1 items-center mt-1.5">
-                <StarSolidIcon className="w-3 h-3 rating-icon" />
-                <span className="text-sm pt-[1px]">
+            <div className="w-full px-10 md:px-28 lg:px-0 lg:mt-2">
+              <h1 className="text-xl md:text-3xl font-medium">
+                {data.title ?? data.name}
+              </h1>
+              <h3 className="text-neutral-700 dark:text-neutral-400 mt-1">
+                {data.tagline}
+              </h3>
+              <div className="flex gap-1 items-center mt-1.5 md:mt-3">
+                <StarSolidIcon className="w-3 h-3 md:w-5 md:h-5 rating-icon" />
+                <span className="text-sm md:text-base pt-[1px]">
                   {data.vote_average.toFixed(1)}
                 </span>
               </div>
-              
-              <div className="flex flex-wrap gap-2 mt-2">
+
+              <div className="flex flex-wrap gap-2 mt-2 md:mt-4">
                 {data.genres.map((genre) => (
                   <div className="text-sm py-1 px-3 bg-gray-200 dark:bg-neutral-800 rounded-3xl">
                     {genre.name}
@@ -63,14 +74,25 @@ const Details = ({ type }) => {
               </div>
             </div>
           </div>
-          <div className="flex flex-col w-full mt-5">
-            <div>
-              <div className="px-10">
-                <h4 className="font-medium">Synopsis</h4>
-                <p className="text-neutral-500 mt-2">{data.overview}</p>
+          <div className="flex flex-col lg:flex-row gap-6 w-full mt-5 md:mt-8 lg:mt-16 lg:px-36">
+            <div className="flex flex-col gap-4 lg:min-w-[200px] px:10 md:px-28 lg:px-2">
+              <div className="flex flex-row lg:flex-col gap-2 items-center lg:items-start">
+              <h4 className="font-medium">Runtime:</h4>
+              <p className="text-neutral-500 mt-1">{convertMinsToHrsMins(data.runtime)}</p>
+              </div>
+              <div className="flex flex-row lg:flex-col gap-2 items-center lg:items-start">
+              <h4 className="font-medium">Release Date:</h4>
+              <p className="text-neutral-500 mt-1">{modifyDate(data.release_date)}</p>
               </div>
 
-              <div className="flex flex-col gap-3 mt-4">
+            </div>
+            <div className="overflow-hidden">
+              <div className="px-10 md:px-28 lg:px-0">
+                <h4 className="font-medium">Synopsis</h4>
+                <p className="text-neutral-500 mt-2 md:mt-5">{data.overview}</p>
+              </div>
+
+              <div className="flex flex-col gap-3 md:gap-5 mt-4 md:mt-8">
                 <CreditsList data={credits.cast} heading="cast" />
                 <CreditsList data={credits.crew} heading="crew" />
               </div>
