598c1657d9d82897bbee5017d1c91ffed6e84171
arshWebDev <arshgoyalDev@gmail.com>
2022-05-28 10:27:27 +0530

    add: link in person card

--- src/components/cards/PersonCard.js
+++ src/components/cards/PersonCard.js
@@ -1,9 +1,15 @@
-import React from "react";
+import { useNavigate } from "react-router-dom";
+
 import { UserIcon } from "../icons";
 
 const PersonCard = ({ data }) => {
+  const navigate = useNavigate();
+
   return (
-    <div className="relative min-w-[120px] xl:min-w-[150px] h-40 xl:h-48 bg-gray-300 dark:bg-neutral-800 rounded-lg overflow-hidden">
+    <div
+      onClick={() => navigate(`/people/${data.id}`)}
+      className="relative min-w-[120px] xl:min-w-[150px] h-40 xl:h-48 bg-gray-300 dark:bg-neutral-800 rounded-lg overflow-hidden cursor-pointer"
+    >
       <div className="absolute z-[1] grid place-items-center w-full h-full">
         {data.profile_path ? (
           <img
@@ -16,7 +22,11 @@ const PersonCard = ({ data }) => {
           <UserIcon className="w-10 h-10 icon" />
         )}
       </div>
-      <div className={`absolute z-[2] w-full h-full flex flex-col gap-2 justify-end p-2 bg-black ${data.profile_path ? "bg-opacity-40" : "bg-opacity-60"}`}>
+      <div
+        className={`absolute z-[2] w-full h-full flex flex-col gap-2 justify-end p-2 bg-black ${
+          data.profile_path ? "bg-opacity-40" : "bg-opacity-60"
+        }`}
+      >
         <div className="flex items-end py-2 px-3 bg-black bg-opacity-30 backdrop-blur-2xl rounded-lg">
           <h4 className="xl:hidden font-medium text-white break-words">
             {data.name.length > 15 ? `${data.name.slice(0, 15)}...` : data.name}
--- src/components/cards/SearchPersonCard.js
+++ src/components/cards/SearchPersonCard.js
@@ -1,8 +1,21 @@
+import { useContext } from "react";
+import { useNavigate } from "react-router-dom";
+import { SearchContext } from "../../context";
+
 import { UserIcon } from "../icons";
 
 const SearchPersonCard = ({ data }) => {
+  const navigate = useNavigate();
+  const { setSearch } = useContext(SearchContext);
+
   return (
-    <div className="relative min-w-[130px] h-44 bg-gray-100 dark:bg-neutral-700 rounded-lg overflow-hidden">
+    <div
+      onClick={() => {
+        navigate(`/people/${data.id}`);
+        setSearch(false);
+      }}
+      className="relative min-w-[130px] h-44 bg-gray-100 dark:bg-neutral-700 rounded-lg overflow-hidden cursor-pointer"
+    >
       <div className="absolute z-[1] grid place-items-center w-full h-full">
         {data.profile_path ? (
           <img
@@ -14,7 +27,11 @@ const SearchPersonCard = ({ data }) => {
           <UserIcon className="w-8 h-8 icon" />
         )}
       </div>
-      <div className={`absolute z-[2] w-full h-full flex flex-col gap-2 justify-end p-2 bg-black ${data.profile_path ? "bg-opacity-40" : "bg-opacity-60"}`}>
+      <div
+        className={`absolute z-[2] w-full h-full flex flex-col gap-2 justify-end p-2 bg-black ${
+          data.profile_path ? "bg-opacity-40" : "bg-opacity-60"
+        }`}
+      >
         <div className="flex items-end py-2 px-3 bg-black bg-opacity-30 backdrop-blur-2xl rounded-lg">
           <h4 className="font-medium text-white break-words">
             {data.name.length > 15 ? `${data.name.slice(0, 15)}...` : data.name}
