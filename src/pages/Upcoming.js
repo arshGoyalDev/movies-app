3bdcc56ec8be2266423ac409afea09f72a40cd85
arshWebDev <arshgoyalDev@gmail.com>
2022-06-01 20:06:48 +0530

    add: content on upcoming page

--- src/pages/Upcoming.js
+++ src/pages/Upcoming.js
@@ -1,6 +1,7 @@
 import { SideBar, NavBar, BottomNav } from "../components/sections";
 
 import OptionsBar from "../components/OptionsBar";
+import List from "../components/List";
 
 const Upcoming = () => {
   return (
@@ -13,7 +14,10 @@ const Upcoming = () => {
         <div>
           <OptionsBar />
 
-          <p>Upcoming</p>
+          <div className="flex flex-col gap-8">
+            <List type="movie" query="upcoming" />
+            <List type="tv" query="airing_today" />
+          </div>
         </div>
 
         <BottomNav />
