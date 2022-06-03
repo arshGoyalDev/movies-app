1eb175bf40fb2589a64b69ec309ebf005fa38b82
arshWebDev <arshgoyalDev@gmail.com>
2022-06-03 12:14:18 +0530

    fix: issue in search

--- src/components/Search/SearchBar.js
+++ src/components/Search/SearchBar.js
@@ -4,15 +4,19 @@ import { SearchContext } from "../../context";
 import { SearchIcon } from "../icons";
 
 const SearchBar = () => {
-  const {setSearch, searchQuery, setSearchQuery} = useContext(SearchContext);
-  
+  const { setSearch, searchQuery, setSearchQuery } = useContext(SearchContext);
+
   const showSearchBox = () => {
+    if (searchQuery.replaceAll(" ", "").length === 0) return;
     setSearch(true);
-  }
+  };
 
   return (
     <div className="relative flex items-center bg-gray-100 dark:bg-neutral-800 rounded-md">
-      <button className="ml-2 w-10 h-10 grid place-items-center" onClick={showSearchBox}>
+      <button
+        className="ml-2 w-10 h-10 grid place-items-center"
+        onClick={showSearchBox}
+      >
         <SearchIcon className="icon w-4 h-4" />
       </button>
       <div className="relative">
@@ -21,7 +25,9 @@ const SearchBar = () => {
           name="search"
           id="search"
           value={searchQuery}
-          onKeyDown={(e) => {if (e.keyCode === 13) showSearchBox()}}
+          onKeyDown={(e) => {
+            if (e.keyCode === 13) showSearchBox();
+          }}
           onChange={(e) => setSearchQuery(e.target.value)}
           className="w-96 py-3.5 px-2 text-sm bg-transparent focus:outline-none"
         />
