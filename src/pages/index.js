da505b6bcf4a6aacaaa5da89e5e4d5767dd7d9c6
arshWebDev <arshgoyalDev@gmail.com>
2022-06-06 17:45:24 +0530

    fix: issue in page

---
+++ src/pages/index.js
@@ -0,0 +1,23 @@
+import Home from "./Home";
+import Movies from "./Movies";
+import TvShows from "./TvShows";
+import People from "./People";
+
+import Upcoming from "./Upcoming";
+import TopRated from "./TopRated";
+import Genres from "./Genres";
+
+import Details from "./Details";
+import PersonDetails from "./PersonDetails";
+
+export {
+  Home,
+  Movies,
+  TvShows,
+  People,
+  Upcoming,
+  TopRated,
+  Genres,
+  Details,
+  PersonDetails,
+};
