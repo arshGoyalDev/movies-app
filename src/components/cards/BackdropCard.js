2a9dcebd9c2d52c7ee054efc13e9593aa289a7a6
arshGoyalDev <arshgoyalDev@gmail.com>
2024-09-17 18:18:20 +0530

    add: new backdrop card

--- src/components/cards/BackdropCard.js
+++ src/components/cards/BackdropCard.js
@@ -1,14 +1,23 @@
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 
+import { useNavigate } from "react-router-dom";
 import { ImageIcon, StarSolidIcon } from "../icons";
+import { useState } from "react";
 
 const BackdropCard = ({ data }) => {
+
+  const navigate  = useNavigate();
+
+  const [visible, setVisible] = useState(false);
   return (
-    <Link
-      to={data.title ? `/movies/${data.id}` : `/tv-shows/${data.id}`}
+    <div
+    onClick={() => {
+      navigate(data.title ? `/movies/${data.id}` : `/tv-shows/${data.id}`);
+    }}
+      // to={data.title ? `/movies/${data.id}` : `/tv-shows/${data.id}`}
       className="relative min-w-[252px] h-36 lg:min-w-[340px] lg:h-48 rounded-2xl cursor-pointer overflow-hidden focus:outline-none"
     >
-      <div className="absolute z-[1] grid place-items-center bg-gray-200 dark:bg-neutral-800 w-full h-full rounded-lg overflow-hidden">
+      <div className="absolute grid place-items-center bg-gray-200 dark:bg-neutral-800 w-full h-full rounded-lg overflow-hidden">
         {data.backdrop_path ? (
           <img
             loading="lazy"
@@ -21,21 +30,21 @@ const BackdropCard = ({ data }) => {
         )}
       </div>
       <div
-        className={`absolute z-[2] flex flex-col ${
-          data.vote_average !== 0 ? "justify-between" : "justify-end"
-        } w-full h-full ${data.backdrop_path ? "bg-black" : ""} ${
-          data.backdrop_path ? "bg-opacity-5 dark:bg-opacity-20" : "bg-opacity-20 dark:bg-opacity-40"
-        }`}
+        className={`absolute z-[10] w-full h-full duration-500 ${
+          data.backdrop_path ? "" : ""
+        } ${data.backdrop_path ? "" : ""}`}
+        onMouseEnter={() => {
+          setVisible(true);
+        }}
+        onMouseLeave={() => {
+          setVisible(false);
+        }}
       >
-        {data.vote_average !== 0 && (
-          <div className="flex items-center gap-2 w-max py-1 px-2 bg-black mt-3 mx-3 rounded-xl">
-            <StarSolidIcon className="w-3 h-3 rating-icon" />
-            <span className="text-white text-xs font-medium pt-0.5">
-              {data.vote_average.toFixed(1)}
-            </span>
-          </div>
-        )}
-        <div className="py-3 px-4 bg-black bg-opacity-20 dark:bg-opacity-30 backdrop-blur-2xl mb-2 mx-2 rounded-xl">
+        <div
+          className={`absolute w-full py-4 px-5 flex flex-col gap-1 ${
+            visible ? "bottom-0" : "-bottom-full"
+          } bg-black bg-opacity-40 backdrop-blur-xl rounded-t-2xl transition-all duration-700`}
+        >
           {data.title ? (
             <h4 className="font-medium text-white">
               {data.title.length > 20
@@ -49,9 +58,16 @@ const BackdropCard = ({ data }) => {
                 : data.name}
             </h4>
           )}
+
+          <div className="flex items-center gap-2 w-max">
+            <StarSolidIcon className="w-3 h-3 rating-icon" />
+            <span className="text-white text-xs font-medium pt-0.5">
+              {data.vote_average.toFixed(1)}
+            </span>
+          </div>
         </div>
       </div>
-    </Link>
+    </div>
   );
 };
 
