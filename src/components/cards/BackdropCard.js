6a12fbd4f11a23d3d70e8bdff06cd93368d938eb
arshGoyalDev <arshgoyalDev@gmail.com>
2024-09-18 21:03:27 +0530

    fix: card for mobile devices

--- src/components/cards/BackdropCard.js
+++ src/components/cards/BackdropCard.js
@@ -14,7 +14,6 @@ const BackdropCard = ({ data }) => {
     onClick={() => {
       navigate(data.title ? `/movies/${data.id}` : `/tv-shows/${data.id}`);
     }}
-      // to={data.title ? `/movies/${data.id}` : `/tv-shows/${data.id}`}
       className="relative min-w-[252px] h-36 lg:min-w-[340px] lg:h-48 rounded-2xl cursor-pointer overflow-hidden focus:outline-none"
     >
       <div className="absolute grid place-items-center bg-gray-100 dark:bg-neutral-900 w-full h-full rounded-lg overflow-hidden">
@@ -41,8 +40,8 @@ const BackdropCard = ({ data }) => {
         }}
       >
         <div
-          className={`absolute w-full py-4 px-5 flex flex-col gap-1 ${
-            visible ? "bottom-0" : "-bottom-full"
+          className={`absolute w-full py-2 px-3 xl:py-4 xl:px-5 flex flex-col xl:flex-row xl:justify-between xl:items-center gap-1 ${
+            visible ? "bottom-0" : "bottom-0 xl:-bottom-full"
           } bg-black bg-opacity-40 backdrop-blur-xl rounded-t-2xl transition-all duration-700`}
         >
           {data.title ? (
